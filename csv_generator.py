42230c56597fbf4cfdc65461069a9015632f51b8
michaelrizzo2 <m_rizzo@live.com>
2020-08-27 21:55:32 -0400

    fixed variable error.

--- csv_generator.py
+++ csv_generator.py
@@ -5,7 +5,7 @@ my_file=open("symbols.txt","r")
 for line in my_file:
     stocks.append(line.strip())
 
-f.close()
+my_file.close()
 
 web.DataReader(stocks,"yahoo",start="2000-1-1",end='2020-8-27')["Adj Close"].to_csv("prices.csv")
 web.DataReader(stocks,"yahoo",start="2000-1-1",end='2020-8-27')["Volume"].to_csv("volume.csv")
