e3a2a04e913eaddd6f7360078eb0bf53d7e64636
michaelrizzo2 <m_rizzo@live.com>
2020-08-27 21:54:21 -0400

    Begginning of the project

---
+++ csv_generator.py
@@ -0,0 +1,11 @@
+import pandas_datareader as web
+
+stocks=[]
+my_file=open("symbols.txt","r")
+for line in my_file:
+    stocks.append(line.strip())
+
+f.close()
+
+web.DataReader(stocks,"yahoo",start="2000-1-1",end='2020-8-27')["Adj Close"].to_csv("prices.csv")
+web.DataReader(stocks,"yahoo",start="2000-1-1",end='2020-8-27')["Volume"].to_csv("volume.csv")
