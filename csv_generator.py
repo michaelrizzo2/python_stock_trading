5f26f20801331169c9adfef5830374cfc3e3e9a7
michaelrizzo2 <m_rizzo@live.com>
2020-08-28 13:16:58 -0400

    Got issue fixed.

--- csv_generator.py
+++ csv_generator.py
@@ -1,11 +1,16 @@
-import pandas_datareader as web
+from pandas_datareader import data
 
 stocks=[]
-my_file=open("symbols.txt","r")
-for line in my_file:
-    stocks.append(line.strip())
+#my_file=open("symbols.txt","r")
+#for line in my_file:
+#    stocks.append(line.strip())
 
-my_file.close()
+#my_file.close()
 
-web.DataReader(stocks,data_source='yahoo',start="2000-1-1",end='2020-8-27')["Adj Close"].to_csv("prices.csv")
-web.DataReader(stocks,data_source='yahoo',start="2000-1-1",end='2020-8-27')["Volume"].to_csv("volume.csv")
+#df1=data.get_data_yahoo("AMZN",start="2000-1-1",end='2020-8-27')["Adj Close"]
+#.to_csv("prices.csv")
+df2=data.get_data_yahoo("AMZN",start="2000-1-1",end='2020-8-27')["Volume"]
+#.to_csv("volume.csv")
+print (df2)
+#df1=web.DataReader(stocks,'yahoo',start="2000-1-1",end='2020-8-27')["Adj Close"].to_csv("prices.csv")
+#df2=web.DataReader(stocks,'yahoo',start="2000-1-1",end='2020-8-27')["Volume"].to_csv("volume.csv")
---
+++ csv_getter.py
@@ -0,0 +1,2 @@
+from pandas_datareader import data
+
