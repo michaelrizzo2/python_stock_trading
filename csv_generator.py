713bd64d730ee25b82209c523c7a41fc9ccc4bb5
michaelrizzo2 <m_rizzo@live.com>
2020-08-28 14:11:21 -0400

    made csv generator work.

--- csv_generator.py
+++ csv_generator.py
@@ -1,13 +1,13 @@
 from pandas_datareader import data
 
 stocks=[]
-#my_file=open("symbols.txt","r")
-#for line in my_file:
-#    stocks.append(line.strip())
+my_file=open("symbols.txt","r")
+for line in my_file:
+    stocks.append(line.strip())
+my_file.close()
 
-#my_file.close()
-
-df1=data.get_data_yahoo("AMZN",start="2000-1-1",end='2020-8-27')["Adj Close"].to_csv("prices.csv")
-df2=data.get_data_yahoo("AMZN",start="2000-1-1",end='2020-8-27')["Volume"] .to_csv("volume.csv")
+for stock in stocks:
+    df1=data.get_data_yahoo(stock,start="2000-1-1",end='2020-8-27')["Adj Close"].to_csv(f"prices_{stock}.csv")
+    df2=data.get_data_yahoo(stock,start="2000-1-1",end='2020-8-27')["Volume"] .to_csv(f"volume_{stock}.csv")
 #df1=web.DataReader(stocks,'yahoo',start="2000-1-1",end='2020-8-27')["Adj Close"].to_csv("prices.csv")
 #df2=web.DataReader(stocks,'yahoo',start="2000-1-1",end='2020-8-27')["Volume"].to_csv("volume.csv")
