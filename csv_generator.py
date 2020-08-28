bce394a83ea544bb73f40d66f31895e3202a10f4
Michael Rizzo <m_rizzo@live.com>
2020-08-28 15:32:28 -0400

    Removed old files.

--- csv_generator.py
+++ csv_generator.py
@@ -7,7 +7,7 @@ for line in my_file:
 my_file.close()
 
 for stock in stocks:
-    df1=data.get_data_yahoo(stock,start="2000-1-1",end='2020-8-27')["Adj Close"].to_csv(f"prices_{stock}.csv")
-    df2=data.get_data_yahoo(stock,start="2000-1-1",end='2020-8-27')["Volume"] .to_csv(f"volume_{stock}.csv")
+    df1=data.get_data_yahoo(stock,start="2000-1-1",end='2020-8-27')["Adj Close"].to_csv(prices.csv")
+    df2=data.get_data_yahoo(stock,start="2000-1-1",end='2020-8-27')["Volume"] .to_csv("volume.csv")
 #df1=web.DataReader(stocks,'yahoo',start="2000-1-1",end='2020-8-27')["Adj Close"].to_csv("prices.csv")
 #df2=web.DataReader(stocks,'yahoo',start="2000-1-1",end='2020-8-27')["Volume"].to_csv("volume.csv")
